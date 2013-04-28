a6dcd7b1d58581b3ca46efc78ebf8df7bd095d00
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-28 17:11:23 -0400

    getters and setters again

--- src/Ability.java
+++ src/Ability.java
@@ -39,4 +39,104 @@ public class Ability {
 		
 		manaCost =mc;
 	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getDamage() {
+		return damage;
+	}
+
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+
+	public int getRange() {
+		return range;
+	}
+
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+
+	public int getManaCost() {
+		return manaCost;
+	}
+
+
+	public void setManaCost(int manaCost) {
+		this.manaCost = manaCost;
+	}
+
+
+	public double getChargeTime() {
+		return chargeTime;
+	}
+
+
+	public void setChargeTime(double chargeTime) {
+		this.chargeTime = chargeTime;
+	}
+
+
+	public boolean isStun() {
+		return stun;
+	}
+
+
+	public void setStun(boolean stun) {
+		this.stun = stun;
+	}
+
+
+	public boolean isDash() {
+		return dash;
+	}
+
+
+	public void setDash(boolean dash) {
+		this.dash = dash;
+	}
+
+
+	public boolean isKnockback() {
+		return knockback;
+	}
+
+
+	public void setKnockback(boolean knockback) {
+		this.knockback = knockback;
+	}
+
+
+	public boolean isHeal() {
+		return heal;
+	}
+
+
+	public void setHeal(boolean heal) {
+		this.heal = heal;
+	}
+
+
+	public Image[] getAbilityParticle() {
+		return abilityParticle;
+	}
+
+
+	public void setAbilityParticle(Image[] abilityParticle) {
+		this.abilityParticle = abilityParticle;
+	}
 }
